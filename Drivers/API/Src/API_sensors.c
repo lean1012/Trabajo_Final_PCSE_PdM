95b6ec969cfee4c9d11dece15774c7c679f0c197
Leandro <diazleandro1012@gmail.com>
2023-04-21 00:57:02 -0300

    add comments

--- Drivers/API/Src/API_sensors.c
+++ Drivers/API/Src/API_sensors.c
@@ -8,28 +8,39 @@
 #include "API_sensors.h"
 #include "sht4x.h"
 #include "sunrise.h"
+#include "API_uart.h"
 
+void init_sensors(void *i2c_init)
+{
 
-
-void init_sensors(void * i2c_init){
-	 sht4x_init(i2c_init);
-	 sunrise_init(i2c_init);
-	 //set_period(2);
-	 //set_number_samples(1);
-
-
+	int8_t err = sht4x_init(i2c_init);
+	if (err < 0)
+	{
+		uint8_t msg[] = "Error en la inicialización del sensor sht4x. \r\n";
+		uartSendStringSize(msg, sizeof(msg));
+	}
+	err = sunrise_init(i2c_init);
+	if (err < 0)
+	{
+		uint8_t msg[] = "Error en la inicialización del sensor sunrise. \r\n";
+		uartSendStringSize(msg, sizeof(msg));
+	}
+	// set_period(2);
+	// set_number_samples(1);
 }
 
-void measurement(struct air * p_air) {
-	//si las lecturas dan con error grabo 0
+void measurement(struct air *p_air)
+{
+	// si las lecturas dan con error grabo 0
 	int8_t err = sunrise_read_co2_filtered(&p_air->co2);
-	if(err<0){
-		p_air->co2=0;
+	if (err < 0)
+	{
+		p_air->co2 = 0;
 	}
 	err = sht4x_temp_hum_low_presition(&p_air->temp, &p_air->hum);
-	if(err<0){
-			p_air->temp=0;
-			p_air->hum=0;
-		}
-
+	if (err < 0)
+	{
+		p_air->temp = 0;
+		p_air->hum = 0;
+	}
 }
